597209ead2eb4c203141cb7d87d3106910dbb84c
Sai Ram <sairamprogramming@gmail.com>
2020-05-13 08:16:12 +0530

    Chapter 1, Examples completed

---
+++ book4/chapter_1/assignment_operators.py
@@ -0,0 +1,25 @@
+# Program demonstrates assignment operators in python.
+
+a, b = 10, 5
+a += b
+print(a)
+
+a, b = 10, 5
+a -= b
+print(a)
+
+a, b = 10, 5
+a *= b
+print(a)
+
+a, b = 10, 5
+a /= b
+print(a)
+
+b, c = 5, 2
+b **= c
+print(b)
+
+b, c = 5, 2
+b //= c
+print(b)
